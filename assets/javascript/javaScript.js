8dc84e63bd4d5ca8652a07cc8b582d2333e0be06
Jayme Howard <jamiewithaY@gmail.com>
2017-08-13 19:53:28 -0500

    mission complete

--- assets/javascript/javaScript.js
+++ assets/javascript/javaScript.js
@@ -1,50 +1,99 @@
 	
+	
 	$(document).ready(function(){
 
-	var targetNumber = Math.floor(Math.random()* (120-19))+19;
-	var score = "";
 
-	$("#target").html(targetNumber);
+	//variables
+
 	var counter = 0;
-	// var arr = []
-	var numberOptions = [1, 3, 5, 9];
-	// var numberIndex = Math.floor(Math.random() * 2);
-	// var increment = numberOptions[numberIndex];
+	var wins= 0;
+	var losses = 0;
 
-	for(var i = 0; i < numberOptions.length; i++){
-		// create an img tag for the gem images
-		var imageCrystal = $("<img>");
-		//create a class selector for the images
-		imageCrystal.addClass("gems");
-		//attach the images
-		imageCrystal.attr("src","http://www.galleries.com/Rock_Crystal/rock_cry.jpg" );
-		//data attribute so I can assign a value for one of these
-		imageCrystal.attr("dataCrystalValue", numberOptions[i]);
-		// add the new images to the existing div id=gemDiv
-		$("#gemDiv").append(imageCrystal);
+	
+
+	function randomNum(min,max){
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max-min +1)) + min;
+
 	}
 
+	var targetNumber = randomNum(40,90);
+	$("#target").html(targetNumber);
 
-	$(".gems").on("click", function(){
+	var crystalOne= randomNum(1, 12);
+	var crystalTwo= randomNum(1, 12);
+	var crystalThree= randomNum(1, 12);
+	var crystalFour= randomNum(1, 12);
+
+
+	function reset(){
+		counter = 0;
+		$("#target").html(targetNumber);
+		targetNumber = randomNum(40,90);
+		$("#playerScore").text(counter);
+		crystalOne= randomNum(1, 12);
+		crystalTwo= randomNum(1, 12);
+		crystalThree= randomNum(1, 12);
+		crystalFour= randomNum(1, 12);
+
+
+	}
+
+	
+	$("#greenGem").on("click", function(){
+		counter += crystalOne;
+		WinLoose();
+		$(playerScore).text(counter);
 		
-		// this is the jQuery version of the thing that got clicked
-		var increment =$(this).attr("dataCrystalValue");
-		counter += parseInt(increment);
+	});
 
-		$("#playerScore").html(increment);
-
-		if (increment == targetNumber){
-		alert("You won!");
-	} else if (increment > targetNumber){
-		alert("You lost");
-	}
+	$("#whiteGem").on("click", function(){
+		counter += crystalTwo;
+		WinLoose();
+		$(playerScore).text(counter);
+		
 
 	});
+
+	$("#yellowGem").on("click", function(){
+		counter += crystalThree;
+		WinLoose();
+		$(playerScore).text(counter);
+		
+
+	});
+
+	$("#redGem").on("click", function(){
+		counter += crystalFour;
+		WinLoose();
+		$(playerScore).text(counter);
+		
+
+	});
+
+	function WinLoose (){
+
+	if (counter === targetNumber){
+		wins += 1;
+		alert("Winner!");
+		$(win).text(wins);
+		reset();
+
+	} else if(counter > targetNumber){
+		losses += 1;
+		alert("You suck sooo bad!");
+		$(loss).text(losses);
+		reset();
+	}
+	
+}
+	
+}); //This goes to the ready function. Leave it alone. 
+
+
 
 
 	
 
 
-
-
-}); //This goes to the ready function. Leave it alone. 
